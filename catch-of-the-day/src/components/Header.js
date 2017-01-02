c2cdb19cc846c5792405e2515bb26caeceeb8098
Chiemeka Ihebom <emekavilla@gmail.com>
2017-01-02 12:10:59 -0500

    Vid 8 stateless component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    console.log(this);
-    return (
-      <header className="top">
+
+const Header = (props) => {
+  return (
+    <header className="top">
         <h1>
         Catch
         <span className="ofThe">
@@ -13,10 +12,10 @@ class Header extends React.Component {
         </span>
         Day
         </h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-        </header>
-    )
-  }
+       <h3 className="tagline"><span>{props.tagline}</span></h3>
+    </header>
+  )
 }
+
 
 export default Header;
