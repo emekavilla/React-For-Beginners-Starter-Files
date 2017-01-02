5b3504b640c122fda351ae17f1ee00bb0f7e55ff
Chiemeka Ihebom <emekavilla@gmail.com>
2017-01-02 18:04:42 -0500

    Vid 13 State

--- catch-of-the-day/src/components/Storepicker.js
+++ catch-of-the-day/src/components/Storepicker.js
@@ -11,15 +11,15 @@ class StorePicker extends React.Component {
   console.log('You changed the URL');
   // first grab the text from the box
   const storeId = this.storeInput.value;
-  console.log('Going to $[storeId]')
+  console.log(`Going to ${storeId}`)
   // second we transition from / to /store/:storeId
-  this.context.router.transitionTo('/store/$[storeId]');
+  this.context.router.transitionTo(`/store/${storeId}`);
  }
 
  render() {
 
    return (
-      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
          <h2>Please Enter A Store</h2>
          <input type="text" required placeholder="Store Name"
            defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
@@ -31,7 +31,7 @@ class StorePicker extends React.Component {
 }
 
 StorePicker.contextTypes = {
-  router: React.Prototypes.object
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
